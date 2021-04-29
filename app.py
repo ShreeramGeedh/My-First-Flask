1bd436ca533de8237ab7be93385020ba499ec52a
Shreeram Geedh <72985123+ShreeramGeedh@users.noreply.github.com>
2021-04-29 19:38:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
 import datetime
+import os
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -13,19 +13,19 @@ def create_app():
 
     @app.route("/", methods=["GET", "POST"])
     def home():
-        print([e for e in app.db.entries.find({})])
         if request.method == "POST":
             entry_content = request.form.get("content")
-            formated_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert({"content": entry_content, "date": formated_date})
-        
-        entries_with_dates = [
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
             (
-                entry["content"],
-                entry["date"],
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            entry["content"],
+            entry["date"], 
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
             )
             for entry in app.db.entries.find({})
         ]
-        return render_template("home.html", entries=entries_with_dates)
+        return render_template("home.html", entries=entries_with_date)
+
     return app
